65313119fd573c28282a7c6baf9243c3ef6ad702
WizardOzzy <121866492+WizardOzzy@users.noreply.github.com>
2023-06-08 15:50:53 +0300

    Add files via upload

--- test_positive_passchange.py
+++ test_positive_passchange.py
@@ -8,25 +8,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from settings import *
+from test_positive_reg import test_registration
 
 
-otvetlist = 'ip90m9mpc@qiott.com'
-login = 'ip90m9mpc'
-domain = 'qiott.com'
 
-def test_pass_change(Rostelecom):
+def test_pass_change(Rostelecom, test_registration):
 
 
 
     pytest.driver.find_element(By.ID, 'forgot_password').click()
     pytest.driver.find_element(By.ID, 't-btn-tab-mail').click()
-    pytest.driver.find_element(By.ID, 'username').send_keys(otvetlist)
+    pytest.driver.find_element(By.ID, 'username').send_keys(test_registration.otvetlist)
     time.sleep(20)
     pytest.driver.find_element(By.ID, 'reset').click()
 
     time.sleep(10)
 
-    response = requests.get(f'https://www.1secmail.com/api/v1/?action=getMessages&login={login}&domain={domain}')
+    response = requests.get(f'https://www.1secmail.com/api/v1/?action=getMessages&login={test_registration.login}&domain={test_registration.domain}')
 
     messages = response.json()
 
@@ -37,25 +35,24 @@ def test_pass_change(Rostelecom):
     time.sleep(5)
 
     response = requests.get(
-        f'https://www.1secmail.com/api/v1/?action=readMessage&login={login}&domain={domain}&id={meskey}')
+        f'https://www.1secmail.com/api/v1/?action=readMessage&login={test_registration.login}&domain={test_registration.domain}&id={test_registration.meskey}')
     text = response.json()
     print(text)
     time.sleep(5)
     soup = BeautifulSoup(text['body'], 'html.parser')
-    soupp = soup.p.text.split(': ')[1].strip().split('.')[0]
-    souppp = list(soupp)
-    print(souppp)
+    delenieotvet = soup.p.text.split(': ')[1].strip().split('.')[0]
+    messaglist = list(delenieotvet)
+    print(messaglist)
 
-    pytest.driver.find_element(By.ID, 'rt-code-0').send_keys(souppp[0])
-
-    pytest.driver.find_element(By.ID, 'rt-code-1').send_keys(souppp[1])
-
-    pytest.driver.find_element(By.ID, 'rt-code-2').send_keys(souppp[2])
-
-    pytest.driver.find_element(By.ID, 'rt-code-3').send_keys(souppp[3])
-
-    pytest.driver.find_element(By.ID, 'rt-code-4').send_keys(souppp[4])
-
-    pytest.driver.find_element(By.ID, 'rt-code-5').send_keys(souppp[5])
+    for i, x in enumerate(messaglist):
+        pytest.driver.find_element(By.ID, f'rt-code-{i}').send_keys(x)
 
     time.sleep(15)
+
+    pytest.driver.find_element(By.ID, 'password-new').send_keys(newpass)
+
+    pytest.driver.find_element(By.ID, 'password-confirm').send_keys(newpass)
+
+    pytest.driver.find_element(By.ID, 't-btn-reset-pass').click()
+
+    pytest.driver.find_element(By.ID, 'password').send_keys(newpass)
